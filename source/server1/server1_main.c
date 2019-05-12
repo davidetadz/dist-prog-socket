9b7cfec610ff51688cd98fec6ebe61ce262b119d
Davide Taddei <taddei.davide@icloud.com>
2019-05-12 20:07:43 +0200

    Added support for multiple requests in same connection (to test)

--- source/server1/server1_main.c
+++ source/server1/server1_main.c
@@ -66,7 +66,7 @@ void sigHandler(int sig_n) {
 int readreq	(int s, char *str) {
 	ssize_t nread;
 
-	nread = Recv(s, str, BUF_SIZE, 0);
+	nread = recv(s, str, BUF_SIZE, 0);
 
 	return nread;
 }
@@ -104,18 +104,231 @@ time_t getFileTimestamp(const char *filename)
 	return st.st_mtime;
 }
 
+int handleRequest(int conn_fd, const char* buf, int read_buf_len) {
+
+    int filename_len;
+    uint32_t f_timestamp, f_size;
+
+	/* Check request format */
+	if (strncmp(read_buf, PROT_REQ, PROT_REQ_LEN) != 0 ||
+		(read_buf[read_buf_len - 2] != '\r') ||
+		(read_buf[read_buf_len - 1] != '\n'))
+	{
+		/* Wrong format */
+		senderr(conn_fd);
+
+		Close(conn_fd);
+		#if DEBUG
+		printf("warning: Format not correct, skipping this request\n");
+		fprintf(f_log, "%s", read_buf);
+		#endif
+
+		return -1;
+	}
+
+	/* Copy filename into another variable */
+	filename_len = read_buf_len - (PROT_REQ_LEN + 2);
+	file_name = (char *)malloc(sizeof(char) * (filename_len + 1));
+
+	memcpy(file_name, read_buf + PROT_REQ_LEN, filename_len);
+	file_name[filename_len] = '\0';
+
+	// Check if filename is valid to prevent going up in directories tree
+	if (strstr(file_name, "../") != NULL)
+	{
+		#if DEBUG
+		printf("warning: Filename not valid, skipping this request\n");
+		fprintf(f_log, "%s", read_buf);
+		#endif
+
+		senderr(conn_fd);
+		Close(conn_fd);
+		free(file_name);
+		file_name = NULL;
+		return -1;
+	}
+
+	// Check if file exists
+	if (access(file_name, R_OK) == -1)
+	{
+		#if DEBUG
+		printf("warning: File not existing or not readable, skipping this request\n");
+		#endif
+
+		senderr(conn_fd);
+		Close(conn_fd);
+		free(file_name);
+		file_name = NULL;
+		return -1;
+	}
+
+	// Read file stats
+	f_size = getFileSize(file_name);
+	f_timestamp = getFileTimestamp(file_name);
+
+	// Check file stats
+	if (f_size < 0)
+	{
+		#if DEBUG
+		printf("warning: cannot read file size, skipping this request\n");
+		#endif
+
+		senderr(conn_fd);
+		Close(conn_fd);
+		free(file_name);
+		file_name = NULL;
+		return -1;
+	}
+
+	if (f_timestamp < 0)
+	{
+		#if DEBUG
+		printf("warning: cannot read last file edit timestamp, skipping this request\n");
+		#endif
+
+		senderr(conn_fd);
+		Close(conn_fd);
+		free(file_name);
+		file_name = NULL;
+		return -1;
+	}
+
+	#if DEBUG
+	printf("info: File \'%s\' OK\n", file_name);
+	printf("info: size %d timestamp %d\n", f_size, f_timestamp);
+	#endif
+
+	f_size = htonl(f_size);
+	f_timestamp = htonl(f_timestamp);
+
+	// Open the file in read mode
+	f = fopen(file_name, "r");
+
+	// Check if open successful
+	if (f == NULL)
+	{
+		#if DEBUG
+		printf("warning: cannot open file, skipping this request\n");
+		#endif
+
+		senderr(conn_fd);
+		Close(conn_fd);
+		free(file_name);
+		file_name = NULL;
+		return -1;
+	}
+
+	// Send the first part of protocol response and check if it's successful
+	if (sendn(conn_fd, PROT_RES, PROT_RES_LEN, 0) != PROT_RES_LEN)
+	{
+		#if DEBUG
+		printf("error: cannot send response header to client, skipping this request\n");
+		#endif
+
+		senderr(conn_fd);
+		Close(conn_fd);
+		free(file_name);
+		fclose(f);
+		f = NULL;
+		file_name = NULL;
+		return -1;
+	}
+
+	// Send file size
+	if (sendn(conn_fd, &f_size, 4, 0) != 4)
+	{
+		#if DEBUG
+		printf("error: cannot send file size to client, skipping this request\n");
+		#endif
+
+		senderr(conn_fd);
+		Close(conn_fd);
+		free(file_name);
+		fclose(f);
+		f = NULL;
+		file_name = NULL;
+		return -1;
+	}
+
+	// Revert file size to system order
+	f_size = ntohl(f_size);
+
+	// Send file content
+
+	//uint32_t bytes_sent = sendfile(conn_fd, fileno(f), NULL, BUF_SIZE);
+
+	uint32_t total_bytes_sent = 0, bytes_sent = 0, bytes_read = 0;
+	send_buf = (char *)malloc(BUF_SIZE * sizeof(char));
+
+	do
+	{
+		bytes_read = fread(send_buf, sizeof(char), BUF_SIZE, f);
+		bytes_sent = sendn(conn_fd, send_buf, bytes_read, 0);
+		total_bytes_sent += bytes_sent;
+	} while (bytes_sent > 0 && !feof(f));
+
+	// Check if file sent successful
+	if (total_bytes_sent != f_size)
+	{
+		#if DEBUG
+		printf("error: while sending file to client! difference %d\n", total_bytes_sent - f_size);
+		printf("[%s]\n\n", strerror(errno));
+		#endif
+
+		senderr(conn_fd);
+		Close(conn_fd);
+		free(file_name);
+		fclose(f);
+		free(send_buf);
+		send_buf = NULL;
+		f = NULL;
+		file_name = NULL;
+		return -1;
+	}
+
+	free(send_buf);
+	send_buf = NULL;
+	fclose(f);
+	f = NULL;
+
+	free(file_name);
+	file_name = NULL;
+
+	#if DEBUG
+	printf("info: file sent successful to client\n");
+	#endif
+
+	// Send the last part of protocol response and check if it's successful
+	if (sendn(conn_fd, &f_timestamp, 4, 0) != 4)
+	{
+		#if DEBUG
+		printf("error: cannot send timestamp to client, closing socket\n");
+		#endif
+
+		senderr(conn_fd);
+		Close(conn_fd);
+		free(file_name);
+		file_name = NULL;
+		return -1;
+	}
+
+	#if DEBUG
+	printf("info: timestamp sent successful to client\n");
+	#endif
+
+	return 1;
+}
+
 int main (int argc, char *argv[])
 {
 	/* Declare local variables */
-	int listen_fd, conn_fd, filename_len, read_buf_len;
+	int listen_fd, conn_fd, read_buf_len;
 	struct sockaddr_in server_addr, client_addr;
 	short port;
 
-	uint32_t f_timestamp, f_size;
-
 	socklen_t client_addrlen = sizeof(client_addr);
 
-	int keep_listening = 1;
+	int res;
 
 	/* Neeeded by error library */
 	prog_name = argv[0];
@@ -166,7 +379,7 @@ int main (int argc, char *argv[])
 	/* Allocate buffers */
 	read_buf = (char *)malloc(BUF_SIZE * sizeof(char));
 
-	while(keep_listening) {
+	while(1) {
 		
 		#if DEBUG
 			printf("info: Waiting for connections...\n");
@@ -175,7 +388,7 @@ int main (int argc, char *argv[])
 		// TODO: Insert select for non-blocking wait condition
 		conn_fd = accept(listen_fd, (SA *)&client_addr, &client_addrlen);
 
-		//TODO: Check timeout
+		// Set socket timeout and check correctness
 		struct timeval timeout;
 		timeout.tv_sec = 15;
 		timeout.tv_usec = 0;
@@ -192,11 +405,9 @@ int main (int argc, char *argv[])
 			#endif
 		}
 
+		// Check if connection was accepted correctly
 		if (conn_fd < 0)
 		{
-
-			int error = errno;
-
 			#if DEBUG
 				printf("error %d: Failed accepting incoming connection\n", error);
 			#endif
@@ -208,228 +419,17 @@ int main (int argc, char *argv[])
 			#endif
 
 			/* Read file request */
-			read_buf_len = readreq(conn_fd, read_buf);
-
-			/* Check request format */
-			if (strncmp(read_buf, PROT_REQ, PROT_REQ_LEN) != 0 ||
-				(read_buf[read_buf_len - 2] != '\r') ||
-				(read_buf[read_buf_len - 1] != '\n'))
-			{
-				/* Wrong format */
-				senderr(conn_fd);
-				
-				Close(conn_fd);
-				#if DEBUG
-					printf("warning: Format not correct, skipping this request\n");
-					fprintf(f_log, "%s", read_buf);
-				#endif
-				
-				continue;
-			}
-
-			/* Copy filename into another variable */
-			filename_len = read_buf_len - (PROT_REQ_LEN + 2);
-			file_name = (char *)malloc(sizeof(char) * (filename_len + 1));
-			
-			memcpy(file_name, read_buf + PROT_REQ_LEN, filename_len);
-			file_name[filename_len] = '\0';
-
-			// Check if filename is valid to prevent going up in directories tree
-			if (strstr(file_name, "../") != NULL )
-			{
-				#if DEBUG
-					printf("warning: Filename not valid, skipping this request\n");
-					fprintf(f_log, "%s", read_buf);
-				#endif
-
-				senderr(conn_fd);
-				Close(conn_fd);
-				free(file_name);
-				file_name = NULL;
-				continue;
-			}
-
-			// Check if file exists
-			if (access(file_name, R_OK) == -1)
-			{
-				#if DEBUG
-					printf("warning: File not existing or not readable, skipping this request\n");
-				#endif
-
-				senderr(conn_fd);
-				Close(conn_fd);
-				free(file_name);
-				file_name = NULL;
-				continue;
-			}
-
-			// Read file stats
-			f_size = getFileSize(file_name);
-			f_timestamp = getFileTimestamp(file_name);
-
-			// Check file stats
-			if (f_size < 0)
-			{
-				#if DEBUG
-				printf("warning: cannot read file size, skipping this request\n");
-				#endif
-
-				senderr(conn_fd);
-				Close(conn_fd);
-				free(file_name);
-				file_name = NULL;
-				continue;
-			}
-
-			if (f_timestamp < 0)
-			{
-				#if DEBUG
-				printf("warning: cannot read last file edit timestamp, skipping this request\n");
-				#endif
-
-				senderr(conn_fd);
-				Close(conn_fd);
-				free(file_name);
-				file_name = NULL;
-				continue;
-			}
-
-			#if DEBUG
-			printf("info: File \'%s\' OK\n", file_name);
-			printf("info: size %d timestamp %d\n", f_size, f_timestamp);
-			#endif
-
-			f_size = htonl(f_size);
-			f_timestamp = htonl(f_timestamp);
-
-			// Open the file in read mode
-			f = fopen(file_name, "r");
-
-			// Check if open successful
-			if (f == NULL) {
-				#if DEBUG
-				printf("warning: cannot open file, skipping this request\n");
-				#endif
-
-				senderr(conn_fd);
-				Close(conn_fd);
-				free(file_name);
-				file_name = NULL;
-				continue;
-			}
-
-			// Send the first part of protocol response and check if it's successful
-			if (sendn(conn_fd, PROT_RES, PROT_RES_LEN, 0) != PROT_RES_LEN) {
-				#if DEBUG
-				printf("error: cannot send response header to client, skipping this request\n");
-				#endif
-
-				senderr(conn_fd);
-				Close(conn_fd);
-				free(file_name);
-				fclose(f);
-				f = NULL;
-				file_name = NULL;
-				continue;
-			}
-
-			// Send file size
-			if (sendn(conn_fd, &f_size, 4, 0) != 4)
-			{
-				#if DEBUG
-				printf("error: cannot send file size to client, skipping this request\n");
-				#endif
-
-				senderr(conn_fd);
-				Close(conn_fd);
-				free(file_name);
-				fclose(f);
-				f = NULL;
-				file_name = NULL;
-				continue;
-			}
-
-			// Revert file size to system order
-			f_size = ntohl(f_size);
-
-			// Send file content
-			
-			//uint32_t bytes_sent = sendfile(conn_fd, fileno(f), NULL, BUF_SIZE);
-
-			uint32_t total_bytes_sent = 0, bytes_sent = 0, bytes_read = 0;
-			send_buf = (char *)malloc(BUF_SIZE * sizeof(char));
-
 			do {
-				bytes_read = fread(send_buf, sizeof(char), BUF_SIZE, f);
-				bytes_sent = sendn(conn_fd, send_buf, bytes_read, 0);
-				total_bytes_sent += bytes_sent;
-			} while (bytes_sent > 0 && !feof(f));
+				read_buf_len = readreq(conn_fd, read_buf);
 
-			// Check if file sent successful
-			if (total_bytes_sent != f_size)
-			{
-				#if DEBUG
-				printf("error: while sending file to client! difference %d\n", total_bytes_sent - f_size);
-				printf("[%s]\n\n", strerror(errno));
-				#endif
+				//TODO: Handle multiple requests from same connection
+				res = handleRequest(conn_fd, read_buf, read_buf_len);
 
-				senderr(conn_fd);
-				Close(conn_fd);
-				free(file_name);
-				fclose(f);
-				free(send_buf);
-				send_buf = NULL;
-				f = NULL;
-				file_name = NULL;
-				continue;
-			}
-
-			free(send_buf);
-			send_buf = NULL;
-			fclose(f);
-			f = NULL;
-
-			#if DEBUG
-			printf("info: file sent successful to client\n");
-			#endif
-
-			// Send the last part of protocol response and check if it's successful
-			if (sendn(conn_fd, &f_timestamp, 4, 0) != 4)
-			{
-				#if DEBUG
-				printf("error: cannot send timestamp to client, closing socket\n");
-				#endif
-
-				senderr(conn_fd);
-				Close(conn_fd);
-				free(file_name);
-				file_name = NULL;
-				continue;
-			}
-
-			#if DEBUG
-			printf("info: timestamp sent successful to client\n");
-			#endif
+			} while (read_buf_len > 0 && res == 1);
 
 			Close(conn_fd);
-			free(file_name);
-			file_name = NULL;
 		}
 	}
-
-	free(read_buf);
-	read_buf = NULL;
-	if (file_name != NULL) {
-		free(file_name);
-		file_name = NULL;
-	}
-
-	#if DEBUG
-	if (f_log != NULL) {
-		fclose(f_log);
-		f_log = NULL;
-	}
-	#endif
 
 	return 0;
 }
